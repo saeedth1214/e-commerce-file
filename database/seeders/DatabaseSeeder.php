fd7b1fb4f27fe66f046e1e35fb0eb709b4907b5c
saeedsoltani <m.h.soltani1214@gmail.com>
2022-12-26 16:37:45 +0330

    comment some seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserTableSeeder::class,
-            VoucherTableSeeder::class,
-            CategoryTableSeeder::class
+            // VoucherTableSeeder::class,
+            // CategoryTableSeeder::class
         ]);
     }
 }
