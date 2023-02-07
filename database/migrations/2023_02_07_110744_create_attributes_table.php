492af7ecc3e263e80ff5954df4271885b3744556
saeedsoltani <m.h.soltani1214@gmail.com>
2023-02-07 12:05:42 +0330

    table : add attributes table

---
+++ database/migrations/2023_02_07_110744_create_attributes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttributesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attributes', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug', 255);
+            $table->string('name', 255);
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('attributes', function (Blueprint $table) {
+            //
+        });
+    }
+}
---
+++ database/migrations/2023_02_07_111012_create_attributes_values_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttributesValuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attributes_values', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('attribute_id')->constrained('users')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId('file_id')->constrained('files')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('value', 255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('attribute_value', function (Blueprint $table) {
+            //
+        });
+    }
+}
