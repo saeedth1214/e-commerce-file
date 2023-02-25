75ba217e1612836166724aea62b61a377d6403cb
saeedsoltani <m.h.soltani1214@gmail.com>
2023-02-25 09:23:07 +0330

    seeder: add two main category with seeder

--- database/seeders/CategoryTableSeeder.php
+++ database/seeders/CategoryTableSeeder.php
@@ -16,64 +16,64 @@ class CategoryTableSeeder extends Seeder
     {
         // DB::table('categories')->truncate();
         $categories = [
+            [
+                'parent_id' => null,
+                'name' => 'طراحی',
+                'slug' => 'designs'
+            ],
+            [
+                'parent_id' => null,
+                'name' => 'تصاویر',
+                'slug' => 'photose'
+            ],
             // [
-            //     'parent_id' => null,
-            //     'name' => 'طراحی',
-            //     'slug' => 'designs'
+            //     'parent_id' => 1,
+            //     'name' => ' 2 بک گراند',
+            //     'slug' => 'Backgrounds 2',
             // ],
             // [
-            //     'parent_id' => null,
-            //     'name' => 'تصاویر',
-            //     'slug' => 'photose'
+            //     'parent_id' => 1,
+            //     'name' => '2پوستر',
+            //     'slug' => 'Posters 2',
             // ],
-            [
-                'parent_id' => 1,
-                'name' => ' 2 بک گراند',
-                'slug' => 'Backgrounds 2',
-            ],
-            [
-                'parent_id' => 1,
-                'name' => '2پوستر',
-                'slug' => 'Posters 2',
-            ],
-            [
-                'parent_id' => 1,
-                'name' => '2بنر',
-                'slug' => 'Banners 2',
-            ],
-            [
-                'parent_id' => 1,
-                'name' => ' 2لوگو',
-                'slug' => 'Logos 2',
-            ],
-            [
-                'parent_id' => 1,
-                'name' => ' 2کارت تبریک',
-                'slug' => 'Greeting Card 2',
-            ],
+            // [
+            //     'parent_id' => 1,
+            //     'name' => '2بنر',
+            //     'slug' => 'Banners 2',
+            // ],
+            // [
+            //     'parent_id' => 1,
+            //     'name' => ' 2لوگو',
+            //     'slug' => 'Logos 2',
+            // ],
+            // [
+            //     'parent_id' => 1,
+            //     'name' => ' 2کارت تبریک',
+            //     'slug' => 'Greeting Card 2',
+            // ],
 
-            [
-                'parent_id' => 2,
-                'name' => ' 2طبیعت',
-                'slug' => 'Nature 2',
-            ],
-            [
-                'parent_id' => 2,
-                'name' => ' 2غذا و نوشیدنی',
-                'slug' => 'Food & Drink2',
-            ],
+            // [
+            //     'parent_id' => 2,
+            //     'name' => ' 2طبیعت',
+            //     'slug' => 'Nature 2',
+            // ],
+            // [
+            //     'parent_id' => 2,
+            //     'name' => ' 2غذا و نوشیدنی',
+            //     'slug' => 'Food & Drink2',
+            // ],
 
-            [
-                'parent_id' => 2,
-                'name' => '2ورزشی',
-                'slug' => 'Sport2',
-            ],
+            // [
+            //     'parent_id' => 2,
+            //     'name' => '2ورزشی',
+            //     'slug' => 'Sport2',
+            // ],
 
-            [
-                'parent_id' => 2,
-                'name' => '2صنعت و تکلولوژی',
-                'slug' => 'Industry & technology2',
-            ],
+            // [
+            //     'parent_id' => 2,
+            //     'name' => '2صنعت و تکلولوژی',
+            //     'slug' => 'Industry & technology2',
+            // ],
 
         ];
         DB::table('categories')->insert($categories);
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            // UserTableSeeder::class,
-            // VoucherTableSeeder::class,
+            UserTableSeeder::class,
             CategoryTableSeeder::class,
+            // VoucherTableSeeder::class,
             // TagSeeder::class,
             // AttributeTableSeeder::class
         ]);
