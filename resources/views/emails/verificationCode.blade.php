d83ced1a42edd1a1dcabd191222bacc5b92991f5
saeedsoltani <m.h.soltani1214@gmail.com>
2023-03-01 20:49:22 +0330

    feature : add vazir font to verify email blade

--- resources/views/emails/verificationCode.blade.php
+++ resources/views/emails/verificationCode.blade.php
@@ -5,11 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/gh/rastikerdar/vazirmatn@v33.003/Vazirmatn-font-face.css" rel="stylesheet" type="text/css" />
     <title>Document</title>
 
     <style>
         body {
-            font-family: 'Arial';
+            font-family: Vazirmatn, sans-serif;
             font-size: 1rem;
             font-weight: normal;
         }
