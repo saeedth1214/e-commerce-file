fb7245ff6e53cb5d8cc351f5bce4c1dcd4f5da00
saeedsoltani <m.h.soltani1214@gmail.com>
2023-03-04 10:07:48 +0330

    show url domain'

--- resources/views/emails/verificationCode.blade.php
+++ resources/views/emails/verificationCode.blade.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/gh/rastikerdar/vazirmatn@v33.003/Vazirmatn-font-face.css" rel="stylesheet" type="text/css" />
+    <link href="https://cdn.jsdelivr.net/gh/rastikerdar/vazirmatn@v33.003/Vazirmatn-font-face.css" rel="stylesheet"
+        type="text/css" />
     <title>Document</title>
 
     <style>
@@ -70,8 +71,8 @@
 
     <div class="mail-body">
         <span class="icon">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#4974ff" class="bi bi-envelope-at"
-                viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#4974ff"
+                class="bi bi-envelope-at" viewBox="0 0 16 16">
                 <path
                     d="M2 2a2 2 0 0 0-2 2v8.01A2 2 0 0 0 2 14h5.5a.5.5 0 0 0 0-1H2a1 1 0 0 1-.966-.741l5.64-3.471L8 9.583l7-4.2V8.5a.5.5 0 0 0 1 0V4a2 2 0 0 0-2-2H2Zm3.708 6.208L1 11.105V5.383l4.708 2.825ZM1 4.217V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v.217l-7 4.2-7-4.2Z" />
                 <path
@@ -88,7 +89,8 @@
         </p>
         <footer>
             <div>
-                .کلیه خدمات این سایت متعلق به <a href='https://filymo.ir'></a> می باشد
+                .می باشد <a href='https://filymo.ir' style="text-decoration: none;color:#4472f1;font-weight: 600">filymo.ir</a>
+                کلیه خدمات این سایت متعلق به
             </div>
             <ul>
                 <li>
