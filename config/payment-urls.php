e9bdad95446da8fe4334d69c06e7632066d0f294
saeedsoltani <m.h.soltani1214@gmail.com>
2023-03-09 18:18:40 +0330

    bug : change callBackUrl

--- config/payment-urls.php
+++ config/payment-urls.php
@@ -3,10 +3,10 @@
 return [
     'plan' => [
         'afterCallback' => 'https://filymo.ir/front/transaction/verify',
-        'callBackUrl' => 'https://api.filymo.ir/api/frontend/transaction/plan/verify',
+        'callBackUrl' => 'https://filymo.ir/api/frontend/transaction/plan/verify',
     ],
     'order' => [
         'afterCallback' => 'https://filymo.ir/front/transaction/verify',
-        'callBackUrl' => 'https://api.filymo.ir/api/frontend/transaction/order/verify',
+        'callBackUrl' => 'https://filymo.ir/api/frontend/transaction/order/verify',
     ]
 ];
