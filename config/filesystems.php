402f8c6b1b6cfeb06a5aba88a46d962cf8173391
saeedsoltani <m.h.soltani1214@gmail.com>
2023-03-14 09:41:53 +0330

    feature
    add  { liara }  disk to connection to object storage

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,7 +38,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
 
@@ -52,6 +52,15 @@ return [
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
         ],
+        
+        'liara' => [
+            'driver' => 's3',
+            'endpoint' => env('ENDPOINT_URL'),
+            'key' => env('ACCESS_KEY'),
+            'secret' => env('SECRET_KEY'),
+            'region' => env('DEFAULT_REGION'),
+            'bucket' => env('BUCKET_NAME'),
+        ]
 
     ],
 
