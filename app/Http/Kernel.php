00d4051963849d980ba34de2f4a4f2d2739c666c
saeedsoltani <m.h.soltani1214@gmail.com>
2023-03-29 18:15:14 +0330

    throttle back to api

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -43,7 +43,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            'throttle:300:1',
+            'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
