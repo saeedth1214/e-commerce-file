8abe1ffdf4da39e9f3794bc8b2efc6fe41150548
saeedsoltani <m.h.soltani1214@gmail.com>
2023-03-30 21:27:02 +0330

    fix token for sending resetPassword

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Mail\ResetPassword;
 use Illuminate\Support\Facades\Route;
 use App\Mail\VerificationCode;
 
 Route::get('/mailable', function () {
     return new VerificationCode('456598');
+});
+
+Route::get('/forget', function () {
+    return new ResetPassword('asd5asdasd44ad54sd545', 'ss+900@g.com');
 });
