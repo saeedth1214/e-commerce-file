fbdb8a461a00c0a6b7a36ebb9e94c08f5aec72d4
saeedsoltani <m.h.soltani1214@gmail.com>
2023-04-15 15:18:06 +0330

    add command for regenerate download link.

---
+++ app/Console/Commands/RefreshDownloadLink.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\File;
+use App\Traits\FileFullPath;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+
+class RefreshDownloadLink extends Command
+{
+
+    use FileFullPath;
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'refresh:link';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This Command Re-Generate Download Link of  File';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $files = File::query()->all();
+
+        foreach ($files as $file) {
+
+            $fullPath = $this->ResolveFileFullPath($file);
+
+            $url = Storage::temporaryUrl($fullPath, now()->addSeconds(604800));
+
+            $file->update([
+                'link' => $url
+            ]);
+
+            // RegenerateDownloadLink::dispatch($file);
+        }
+    }
+}
