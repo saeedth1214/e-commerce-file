0f978a010fcc160c7ae9ebc2c5f60e121d8eb5d5
saeedsoltani <m.h.soltani1214@gmail.com>
2023-04-20 11:45:18 +0330

    add refresh link route

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,12 @@
 <?php
 
-use App\Mail\ResetPassword;
 use Illuminate\Support\Facades\Route;
 use App\Mail\VerificationCode;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/mailable', function () {
     return new VerificationCode('456598');
 });
-
-Route::get('/forget', function () {
-    return new ResetPassword('asd5asdasd44ad54sd545', 'ss+900@g.com');
+Route::get('/refresh-link', function () {
+    Artisan::call('refresh:link');
 });
